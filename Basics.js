0938122d3084e7abd4c1d9021488a1b2c6ddbf11
naveenreddy964 <68942061+naveenreddy964@users.noreply.github.com>
2020-11-05 16:36:23 +0530

    Update Basics.js

--- Basics.js
+++ Basics.js
@@ -3,7 +3,7 @@ var b = "ajfab";
 var c = 4;
 var list = [1, 2, 3, 4];
 var dynamic;
-dynamic = 1;
+dynamic = 2;
 dynamic = "aja";
 var _loop_1 = function (i) {
     setTimeout(function () { console.log(i); }, 100 * i);
